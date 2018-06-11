dfa71fb4467ab3f8d97588c7c351bcda769e2242
zljing <zljing@codeaurora.org>
2018-06-11 21:39:44 +0530

    prop: Shorten wait time to optimize shutdown time
    Mountservice will check if there exist processes occupy files. Launch
    many 3rd apps and set in background will cause many processes occupy
    files, which will cause mountservice spend 10~20s to unmount(sometimes
     also unmount fail with 20s time out). If there is no process occupy
    files, mountservice only need about 500ms. So shorten wait time from
    20s to 500ms, which will save 10~20s for shutdown time, total shutdown
    time is in 3s.
    
    Change-Id: Ida59f99e1c3c1f1448001b4e5a2621a89de7bb11
    Signed-off-by: Nick <nickgunale@gmail.com>
    Signed-off-by: Sakil_Mondal <mondalsakil@gmail.com>
    Signed-off-by: celtare21 <celtare21@gmail.com>
    Signed-off-by: DennySPB <dennyspb@gmail.com>
    Signed-off-by: rashedsahaji <asifrashed2@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -75,3 +75,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.dbg.volte_avail_ovr=1 \
     persist.dbg.vt_avail_ovr=1 \
     persist.dbg.wfc_avail_ovr=1
+
+# Boot
+PRODUCT_PROPERTY_OVERRIDES += \
+    sys.vendor.shutdown.waittime=500
